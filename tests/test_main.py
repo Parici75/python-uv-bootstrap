81071aa7ef0cb7e0a63c14d1f5aa38eb88824e01
Parici75 <benjamin.roland@hotmail.fr>
2026-01-07 02:38:04 +0100

    ci: add pydocstyle rules

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
+from pytest import LogCaptureFixture
+
 from libs.main import hello_world
 
 
-def test_hello_world(caplog):
+def test_hello_world(caplog: LogCaptureFixture) -> None:
     hello_world()
     assert "Hello world" in caplog.text
